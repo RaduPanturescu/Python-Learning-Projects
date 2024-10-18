d6948adeb294fa2c27b4fa13446742936d8aa095
RaduPanturescu <radupanturescu@gmail.com>
2024-10-18 12:31:50 +0200

    Created TO order class

--- Logistic Picking System.py
+++ Logistic Picking System.py
@@ -2,7 +2,7 @@ import random as r
 import os
 
 # product_code = {"Sol de Janeiro", 9400234596}
-TO_backlog = {}
+TO_backlog = {"938240750":1}
 warehouse_inventory = {}
 product_list = []
 user = "TestUser1"
@@ -10,7 +10,6 @@ user = "TestUser1"
 test_order_1 = {"Sol de Janeiro": 5,
                 "Gucci Flora": 1,
                 "Prada Paradox": 1} 
-
 
 clear_console = lambda: os.system('cls')
 
@@ -33,28 +32,26 @@ def program_initialiser():
                 add_inventory(item, quantity)
 
             elif action == 2:
-                check_inventory()
-            
+                check_inventory()    
     return
 
 # Create an order as a customer for the picking system
 def create_TO(picking_quantity): 
 
-    TO_List = {}
     TO_number = r.randint(900000000,999999999)
     item = ""
     quantity = 0
 
     while True:
         if TO_number not in TO_backlog:
-            TO_backlog[TO_number] = 1
-            print("ok")
-            print(TO_backlog)
+            for TO in TO_backlog:
+                # TO_backlog[TO_number] = int(TO)+1   XXXXX
+                print("ok")
+                print(TO_backlog)
             break 
         else:
             TO_number = r.randint(900000000,999999999)
             
-
     for i in range(picking_quantity):
         if item not in TO_backlog:
             item = input("Item to order - ")
@@ -67,9 +64,7 @@ def create_TO(picking_quantity):
 
     print(TO_backlog)
     print(TO_number)
-
-
-        
+   
 
 # Delete an order
 def remove_TO():
@@ -94,10 +89,18 @@ def pickting_TO():
     return
 
 
+class TO:
+    def __init__(self, creation_date, to_code, order_products, order_quantity):
+        self.date = creation_date
+        self.code = to_code
+        self.products = order_products
+        self.quantity = order_quantity
 
-add_inventory("Sol de Janeiro - Lip Balm", 25)
-# add_inventory("Sol de Janeiro - Body Cream", 25)
-# check_inventory()
+    def print_TO(self):
+        print(f"""TO Created at - {self.date}
+TO code - {self.code}
+Products ordered - {self.products}
+Ammount ordered - {self.quantity}""")
 
-# program_initialiser()
-create_TO(3)
+TO1 = TO("12.02.2024", r.randint(0,1000), "Sex de janeiro", r.randint(1,10))
+TO.print_TO(TO1)
